ddf014c61629d0da071ecd53a6b2be569cca23c6
allisterakun <allisterakun@gmail.com>
2021-05-17 04:22:18 -0700

    done with bug

--- 2021/receiver.py
+++ 2021/receiver.py
@@ -2,6 +2,10 @@
 
 import logging
 
+import string
+import struct
+import zlib
+
 import channelsimulator
 import utils
 import sys
@@ -41,7 +45,78 @@ class BogoReceiver(Receiver):
             except socket.timeout:
                 sys.exit()
 
+class myReceiver(BogoReceiver):
+
+    def __init__(self):
+        super(myReceiver,self).__init__()
+
+    def receive(self):
+        self.logger.info("Receiving on port: {} and replying with ACK on port: {}".format(self.inbound_port, self.outbound_port))
+
+        lower=0
+        termination=False
+
+        received_packets={}
+
+        while ~termination:
+            try:
+                received_packet=self.simulator.u_receive()
+
+                # first check if it is "1011111" -> termination message
+                if received_packet[0] & (~received_packet[1] & 0xFF) & received_packet[2] & received_packet[3] & received_packet[4] & received_packet[5] & received_packet[6] == 255:
+                    self.logger.info("TERMINATION")
+                    # send back "1011111" termination message and terminate
+                    self.simulator.u_send(bytearray([255, 0] + [255]*5))
+                    termination = True
+                else:
+                    received_seq_num_bin=received_packet[0:4]
+                    received_data_len_bin=received_packet[4:8]
+                    received_checksum_bin=received_packet[8:12]
+                    received_data=received_packet[12:]
+
+                    received_seq_num_int=struct.unpack(">i",received_seq_num_bin)[0]
+
+                    self.logger.info("RECEIVED: {}".format(received_seq_num_int))
+
+                    received_seq_num_bin_str=bin(received_seq_num_int)[2:].zfill(32)
+
+                    checksum_bin_str=bin(self.checksum(received_seq_num_bin_str,received_data))
+                    checksum_bin=bytearray(int(checksum_bin_str[i:i+8],2) for i in range(0,32,8))
+
+                    # compare received checksum vs. checksum calculated from received packet
+                    if received_checksum_bin==checksum_bin:
+                        self.logger.info("UNCORRUPTED")
+                        received_packets[received_seq_num_int]=received_data[:struct.unpack(">i",received_data_len_bin)[0]]
+
+                        ones = 1111
+                        return_msg = received_seq_num_bin + struct.pack(">i",ones)
+                        self.simulator.u_send(return_msg)
+                        self.logger.info("Replying ACK {}".format(received_seq_num_int))
+
+                    else:
+                        self.logger.info("CORRUPTED")
+
+                        zeros = 0000
+                        return_msg = received_seq_num_bin + struct.pack(">i",zeros)
+                        self.simulator.u_send(return_msg)
+                        self.logger.info("Replying ACK {}".format(received_seq_num_int))
+            except socket.timeout:
+                print("timeout")
+
+        for key,value in sorted(received_packets.items()):
+            sys.stdout.write(value)
+
+        sys.exit()
+
+
+
+
+    def checksum(self,seq_num_bin_str,data_bin):
+        filled_data = string.join([string.zfill(n, 8) for n in map(lambda s: s[2:], map(bin, data_bin))], '')
+        checksum = zlib.adler32(seq_num_bin_str + filled_data) & 0xffffffff
+        return checksum
+
 if __name__ == "__main__":
     # test out BogoReceiver
-    rcvr = BogoReceiver()
+    rcvr = myReceiver()
     rcvr.receive()
--- 2021/sender.py
+++ 2021/sender.py
@@ -56,7 +56,6 @@ class mySender(BogoSender):
         self.logger.info("Sending on port: {} and waiting for ACK on port: {}".format(self.outbound_port, self.inbound_port))
 
         lower=0
-        # lower_seq_num=0
 
         tuple_array,success = self.prepare_data(data)
         total_packets=len(tuple_array)
@@ -78,22 +77,61 @@ class mySender(BogoSender):
 
                 for i in range(lower_seq_num_int,upper_seq_num_int):
                     if tuple_array[i]["sent"] == False:
-                        datagram = ...
-                        tuple_array[i]["sequence_number"] + ...
-                        tuple_array[i]["data_packet_length"] + ...
-                        tuple_array[i]["checksum"] + ...
-                        tuple_array[i]["data"]
+                        datagram = tuple_array[i]["sequence_number"] + \
+                                   tuple_array[i]["data_packet_length"] + \
+                                   tuple_array[i]["checksum"] + \
+                                   tuple_array[i]["data"]
 
                         self.simulator.u_send(datagram)
                         tuple_array[i]["sent"]=True
+                        self.logger.info("Sent packet with sequence number {}".format(i))
                 
                 while True:
                     return_packet=self.simulator.u_receive()
 
+                    returned_seq_num_bin=return_packet[0:4]
+                    returned_ack_bin=return_packet[4:8]
 
+                    returned_seq_num_int=struct.unpack(">i",returned_seq_num_bin)[0]
+                    returned_ack_int=struct.unpack(">i",returned_ack_bin)[0]
 
-            except socket.timeout:
-                print("timeout")
+                    
+
+                    if  returned_seq_num_int>=lower and returned_seq_num_int<=upper \
+                        and success[returned_seq_num_int] == False \
+                        and (returned_ack_int==1111 or \
+                             returned_ack_int==1110 or \
+                             returned_ack_int==1101 or \
+                             returned_ack_int==1011 or \
+                             returned_ack_int==111):
+                        success[returned_seq_num_int]=True
+                        self.logger.info("Received ACK for packet with sequence number {}".format(returned_seq_num_int))
+                        if returned_seq_num_int==lower:
+                            break
+
+                    # find the next unsuccessful packet and update lower
+                    for i in range(lower,min(upper+1,total_packets-1)):
+                        if not success[i]:
+                            lower = i
+                            break
+                    
+            except socket.timeout as timeoutException:
+                self.logger.info(str(timeoutException))
+                unfinished=False
+                for i in range(lower_seq_num_int,upper_seq_num_int+1):
+                    if ~success[i]:
+                        datagram = tuple_array[i]["sequence_number"] + \
+                                   tuple_array[i]["data_packet_length"] + \
+                                   tuple_array[i]["checksum"] + \
+                                   tuple_array[i]["data"]
+                        self.simulator.u_send(datagram)
+                        unfinished=True
+                        self.logger.info("Resent packet with sequence number {}".format(i))
+                
+                if ~unfinished:
+                    self.logger.info("Finished")
+                    termination=True
+
 
         # done with all packets, time for terminator
         while True:
@@ -144,7 +182,7 @@ class mySender(BogoSender):
 
     def checksum(self,seq_num_bin_str,data_bin):
         filled_data = string.join([string.zfill(n, 8) for n in map(lambda s: s[2:], map(bin, data_bin))], '')
-        checksum = zlib.crc32(seq_num_bin_str + filled_data) & 0xffffffff
+        checksum = zlib.adler32(seq_num_bin_str + filled_data) & 0xffffffff
         return checksum
 
 
@@ -153,5 +191,5 @@ class mySender(BogoSender):
 if __name__ == "__main__":
     # test out BogoSender
     DATA = bytearray(sys.stdin.read())
-    sndr = BogoSender()
+    sndr = mySender()
     sndr.send(DATA)
